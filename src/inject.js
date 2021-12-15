07bab2f7558c2b8728681ba1098cb3a40a8a132f
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2021-12-16 04:25:13 +0500

    filler update completed

--- src/inject.js
+++ src/inject.js
@@ -1,146 +1,203 @@
+function fill_form(ratings, text_message, submit_state) {
+  // Odoo Qalam Nust Feedback Filler
 
+  // Developed By a Mechanic from ME11 SMME NUST
 
+  // value of rating
+  // 2 for Excellent
+  // 0 for Average
+  // 1 for Poor
+  // 3 for Very Good
+  // 4 for Good
+  var ratelabel = ["Average", "Poor", "Excellent", "Very Good", "Good"];
 
-function fill_form(ratings,text_message){
+  // for all very good only
+  // var ratings=[3]
 
-// Odoo Qalam Nust Feedback Filler
+  // for random very good and excellent
+  // var ratings=[2,3]
+  // Fill the value of rating in array from random rating be selected to be filled in rows or keep default.
 
-// Developed By a Mechanic from ME11 SMME NUST
+  // var text_message="..."
 
+  // Instructions
+  // Change Values Above to be filled
+  // Login to Qalam
+  // Go to feedback forms.
+  // Open any unfilled form
+  // Press f12 on keyboard ((or Right click and Inspect Element and go to 'Console' Tab))
+  // and Paste this whole text. and Enter.
+  // And form filled in milli seconds
 
-// value of rating
-// 2 for Excellent
-// 0 for Average
-// 1 for Poor
-// 3 for Very Good
-// 4 for Good
-var ratelabel=['Average','Poor','Excellent','Very Good','Good']
+  var radios = document.getElementsByTagName("input");
+  var textareas = document.getElementsByTagName("textarea");
 
-// for all very good only
-// var ratings=[3]
+  var nochecked = 0;
 
-// for random very good and excellent
-// var ratings=[2,3]
-// Fill the value of rating in array from random rating be selected to be filled in rows or keep default.
+  for (var k = 0; k < 5; k += 1) {
+    var random = Math.floor(Math.random() * ratings.length);
+    // console.log(random);
 
+    var rated = [];
 
+    var ratingnow = ratings[random];
+    var checknow = ratingnow;
+    // console.log(ratingnow);
 
-// var text_message="..."
+    // console.log(checknow);
 
-// Instructions
-// Change Values Above to be filled
-// Login to Qalam
-// Go to feedback forms.
-// Open any unfilled form
-// Press f12 on keyboard ((or Right click and Inspect Element and go to 'Console' Tab))
-// and Paste this whole text. and Enter.
-// And form filled in milli seconds
+    for (var i = 0; i < radios.length / 5; i += 1) {
+      // if(i  == checknow){
 
+      rated.push(checknow);
 
+      //   console.log(rated);
 
+      try {
+        var radio = radios[checknow];
+        radio.checked = true;
+      } catch (e) {
+        // statements to handle any exceptions
+        console.error(e); // pass exception object to error handler
+      }
 
-var radios = document.getElementsByTagName('input');
-var textareas = document.getElementsByTagName('textarea');
+      nochecked = nochecked + 1;
+      console.log(`Checked ${checknow} Radio`);
 
+      checknow = checknow - ratingnow;
+      //   console.log(checknow);
+      checknow = checknow + 5;
+      //   console.log(checknow);
 
+      random = Math.floor(Math.random() * ratings.length);
+      ratingnow = ratings[random];
 
-var nochecked=0
+      //   console.log(ratingnow);
 
-var random = Math.floor(Math.random() * ratings.length);
+      checknow = checknow + ratingnow;
 
-var rated=[]
+      //   console.log(checknow);
+      //   console.log(ratelabel[ratingnow]);
 
-var ratingnow =ratings[random]
-var checknow=ratingnow
+      //  }
+    }
+    console.log(`No. of checked radios ${nochecked}`);
+  }
 
-for (var i = checknow; i < radios.length; i+=1) {
+  for (var i = 0; i < textareas.length; i++) {
+    var textarea = textareas[i];
+    textarea.innerText = text_message;
+    console.log(`Text Area Filled ${text_message}`);
+  }
 
- 
+  if (nochecked > 0) {
+    if (submit_state) {
+      submit_form();
+      //   console.log("To be Submitted");
+    } else {
+      //   console.log(" Not be Submitted");
+    }
 
-    if(i  == checknow){
+    return true;
+  } else {
+    return false;
+  }
+}
 
-rated.push(checknow)
+function submit_form() {
+  var submit_btn = document.getElementsByName("button_submit")[0];
+  // Submit the form
+  setTimeout(() => {
+    submit_btn.click();
+  }, 100);
+}
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    self.focus();
+    // window.focus();
+    // win.focus();
+  }
+
+function open_feedback_forms() {
+  var links = document.querySelectorAll("a[class='md-list-addon-element']");
+
+  var feed_back_links=[]
+  for (var i = 0; i < links.length; i++) {
+    // console.log(links[i]);
+    var href=links[i].href
+    feed_back_links.push(href)
+    // openInNewTab(href)
   
-    var radio =radios[i]
-    if (radio.type == 'radio' && !radio.checked) {
-        radio.checked=true
-        nochecked= nochecked +1
-        console.log(`Checked ${i} Radio`)
-    }else{
-    console.log(`Already Checked ${i} Radio`)
+    // links[i].click();
+  }
+  console.log(feed_back_links)
+  return feed_back_links
+}
+
+function close_tab() {
+  window.close();
+}
+
+var pathArray = window.location.pathname.split("/");
+console.log(pathArray);
+
+(function () {
+  console.log("Odoo Qalam Nust Feedback Filler Injected");
+  console.log("Developed By a Mechanic from ME11 SMME NUST");
+
+  chrome.runtime.onMessage.addListener(function (
+    recieved,
+    sender,
+    sendResponse
+  ) {
+
+
+   
+    var op = recieved.op;
+    var response=false
+    if (pathArray[1] == "survey") {
+      
+
+
+      if (op == 3) {
+        close_tab();
+        response=true
+      }
+
+      if (op == 2) {
+        var ratings = recieved.ratings;
+        var text_message = recieved.text_message;
+        var submit_state = recieved.submit_state;
+
+        console.log(`Recieved: \n Comments: ${text_message} \n Ratings`);
+
+        console.log(ratings);
+
+        console.log(`\n Submit State: ${submit_state}`);
+
+        response = fill_form(ratings, text_message, submit_state);
+
+      }
     }
 
-
-checknow=checknow-ratingnow
-    checknow=checknow+5
-    
-
- random = Math.floor(Math.random() * ratings.length)
- ratingnow =ratings[random]
-
- checknow=checknow+ratingnow
-
-//  console.log(checknow)
- console.log(ratelabel[ratingnow])
+    if (pathArray[3] == "feedback") {
+      
+      if (op == 1) {
+        var feed_back_links = open_feedback_forms();
+        response=feed_back_links
+      }
 
 
- }
+      //Refresh page and get rid of form 
+      if (op == 4) {
+        window.location.reload();
+        response=true
+      }
 
-}
-console.log(`No. of checked radios ${nochecked}`)
-
-for (var i = 0; i < textareas.length; i++) {
-    var textarea =textareas[i]
-    textarea.innerText=text_message
-    console.log(`Text Area Filled ${text_message}`)   
-}
-
-
-if(nochecked > 0){
-
-    return true
-}else{
-    return false
-}
-
-}
-
-
-(function() {
-
-
-
-
-console.log('Odoo Qalam Nust Feedback Filler Injected')
-console.log('Developed By a Mechanic from ME11 SMME NUST')
-
-
-chrome.runtime.onMessage.addListener(
-    function(recieved, sender, sendResponse) {
-
- 
-     
-        var ratings = recieved.ratings
-        var text_message= recieved.text_message
-
-        console.log(`Recieved: \n Comments: ${text_message} \n Ratings`)
-
-        console.log(ratings)
-        
-        filled = fill_form(ratings,text_message)
-
-            sendResponse(filled);
-        
-
-        
-
-
-
+      
     }
-);
-       
 
-        
-
-
+    sendResponse(response);
+  });
 })();
--- src/popup.js
+++ src/popup.js
@@ -1,101 +1,66 @@
-
-
-
 // value of rating
 // 2 for Excellent
 // 0 for Average
 // 1 for Poor
 // 3 for Very Good
 // 4 for Good
-var ratelabel=['Average','Poor','Excellent','Very Good','Good']
+var ratelabel = ["Average", "Poor", "Excellent", "Very Good", "Good"];
 
+var textarea = document.getElementById("comments");
+// var checkboxes = document.getElementsByClassName("ratings");
 
+var checkboxes = document.querySelectorAll("input[class='ratings']");
+var submit_switch = document.getElementById("submit_switch");
 
+var text_message = "...";
+var ratings = [2, 3];
 
-
-var textarea = document.getElementById('comments');
-var checkboxes = document.querySelectorAll("input[type=checkbox]");
-    
-
-
-var text_message="..."
-let ratings = [2,3]
-
-
-textarea.value = text_message
-console.log(text_message)
+textarea.value = text_message;
+console.log(text_message);
 
 for (let i in ratings) {
-  var rating =ratings[i]
-      console.log(rating)
-     
-      document.getElementById(String(rating)).checked = true;
-      
-  
+  var rating = ratings[i];
+  console.log(rating);
+
+  document.getElementById(String(rating)).checked = true;
+}
+
+try {
+  chrome.storage.local.get("qalam_feedback_comments_text", (value) => {
+    var qalam_feedback_comments_text = value.qalam_feedback_comments_text;
+
+    if (qalam_feedback_comments_text != undefined) {
+      text_message = qalam_feedback_comments_text;
+      textarea.value = text_message;
+      console.log(text_message);
     }
+  });
 
+  chrome.storage.local.get("qalam_feedback_ratings", (value) => {
+    var qalam_feedback_ratings = value.qalam_feedback_ratings;
+    console.log(qalam_feedback_ratings);
 
-try{
+    if (qalam_feedback_ratings != undefined) {
+      for (let i = 0; i < 5; i++) {
+        document.getElementById(String(i)).checked = false;
+      }
 
-chrome.storage.local.get("qalam_feedback_comments_text", value =>{
-    var qalam_feedback_comments_text = value.qalam_feedback_comments_text
+      ratings = qalam_feedback_ratings;
 
-    if (qalam_feedback_comments_text != undefined){
-    text_message=qalam_feedback_comments_text
-    textarea.value = text_message
-    console.log(text_message)
+      for (let i in qalam_feedback_ratings) {
+        var rating = qalam_feedback_ratings[i];
+        console.log(rating);
+
+        document.getElementById(String(rating)).checked = true;
+      }
     }
-    
+  });
+} catch {
+  console.log("Cant get Chrome Storage Local Values");
 
-})
-
-
-chrome.storage.local.get("qalam_feedback_ratings", value =>{
-    var qalam_feedback_ratings = value.qalam_feedback_ratings
-   console.log(qalam_feedback_ratings)
-
-if(qalam_feedback_ratings != undefined){
-
-for(let i=0; i<5; i++){
-
-  document.getElementById(String(i)).checked = false;
+  chrome.storage.local.set({ qalam_feedback_ratings: ratings });
+  chrome.storage.local.set({ qalam_feedback_comments_text: text_message });
 }
-
-   for (let i in qalam_feedback_ratings) {
-var rating =qalam_feedback_ratings[i]
-    console.log(rating)
-   
-    document.getElementById(String(rating)).checked = true;
-    
-
-  }
-}
-
-
-
-})
-
-
-
-
-}catch{
-
-    console.log('Cant get Chrome Storage Local Values')
-
-    chrome.storage.local.set({"qalam_feedback_ratings":ratings})
-    chrome.storage.local.set({"qalam_feedback_comments_text":text_message})
-
-
-      
-
-}
-
-
-
-
-
-
-
 
 // function getSelectedCheckboxValues(){
 //     const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
@@ -103,102 +68,181 @@ var rating =qalam_feedback_ratings[i]
 //     checkboxes.forEach((checkbox) => {
 //         ratings.push(Number(checkbox.value));
 //     });
-    
+
 //     return ratings
 //     }
-    
-    
 
-    checkboxes.forEach(function(checkbox) {
-      checkbox.addEventListener('change', function() {
-        ratings = 
-          Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
-          .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
-          .map(i => Number(i.value)) // Use Array.map to extract only the checkbox values from the array of objects.
-          
-          chrome.storage.local.set({"qalam_feedback_ratings":ratings})
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    ratings = Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+      .filter((i) => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map((i) => Number(i.value)); // Use Array.map to extract only the checkbox values from the array of objects.
 
-        console.log(ratings)
-      })
-    });
-    
+    chrome.storage.local.set({ qalam_feedback_ratings: ratings });
 
+    console.log(ratings);
+  });
+});
 
+function updateComments() {
+  text_message = textarea.value;
 
+  chrome.storage.local.set({ qalam_feedback_comments_text: text_message });
+}
 
+textarea.addEventListener("keyup", updateComments);
 
+var submit_state = false;
 
+function set_submit_state() {
+  submit_state = submit_switch.checked;
+  console.log(submit_state);
+}
 
+submit_switch.addEventListener("change", set_submit_state);
 
+const fill_all_forms_btn = document.querySelector("#fill_all_forms");
+fill_all_forms_btn.addEventListener("click", (event) => {
+  fill_forms(ratings, text_message, submit_state);
+});
 
+const fill_form_btn = document.querySelector("#fill_form");
+fill_form_btn.addEventListener("click", (event) => {
+  console.log(ratings);
 
+  console.log(text_message);
 
-    function updateComments() {
-        text_message = textarea.value;
-    
-        chrome.storage.local.set({"qalam_feedback_comments_text":text_message})
-    
-    }
-    
-    textarea.addEventListener('keyup', updateComments);
-    
-    
+  console.log(submit_state);
 
+  fill_form(ratings, text_message, submit_state);
+});
 
+const open_all_forms_btn = document.querySelector("#open_all_forms");
+open_all_forms_btn.addEventListener("click", (event) => {
+  open_all_forms();
+});
 
-
-const fill_all_forms_btn = document.querySelector('#fill_all_forms');
-fill_all_forms_btn.addEventListener('click', (event) => {
-
-  fill_forms(ratings,text_message)
-    
-   
+const close_all_forms_btn = document.querySelector("#close_all_forms");
+close_all_forms_btn.addEventListener("click", (event) => {
+  close_all_forms();
 });
 
 
-
-
-
-
-const fill_form_btn = document.querySelector('#fill_form');
-fill_form_btn.addEventListener('click', (event) => {
-
-    console.log(ratings)
-
-    console.log(text_message)
-
-
-
-
-    fill_form(ratings,text_message)
+const rid_all_forms_btn = document.querySelector("#rid_all_forms");
+rid_all_forms_btn.addEventListener("click", (event) => {
+  rid_all_forms();
 });
 
+function rid_all_forms() {
+
+  open_all_forms()
+
+  setTimeout(() => {
+    fill_forms(ratings, text_message, true);
+  }, 10000);
+
+  setTimeout(() => {
+    close_all_forms();
+  }, 20000);
+
+  setTimeout(() => {
+    refresh_feedback_page();
+  }, 25000);
 
 
-
-
-
-
-
-
-function fill_form(ratings,text_message){
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {ratings: ratings,text_message: text_message}, function(response){
-     
-      if (response){
-
-        document.getElementById("response").innerText = "Form Filled"
-
-      }
-
-    });
-});
 
 
 }
 
 
+
+function close_all_forms() {
+  
+
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; ++i) {
+      chrome.tabs.sendMessage(
+        tabs[i].id,
+        {
+          op: 3,
+      
+        },
+        function (response) {
+          console.log(response);
+
+          if (response) {
+            document.getElementById(
+              "response"
+            ).innerText = `All Forms Closed`;
+          }
+        }
+      );
+    }
+  });
+}
+
+function refresh_feedback_page() {
+  
+
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; ++i) {
+      chrome.tabs.sendMessage(
+        tabs[i].id,
+        {
+          op: 4,
+      
+        },
+        function (response) {
+          console.log(response);
+
+          if (response) {
+            document.getElementById(
+              "response"
+            ).innerText = `All Forms Filled`;
+          }
+        }
+      );
+    }
+  });
+}
+
+
+
+function open_all_forms() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { op: 1 }, function (response) {
+      console.log("Opening all forms");
+
+      if (response) {
+        document.getElementById("response").innerText =
+          "Opened All Forms";
+          console.log(response)
+          for(var i=0; i<response.length; i++){
+          chrome.tabs.create({url: response[i], active: false});
+          }
+      }
+    });
+  });
+}
+
+function fill_form(ratings, text_message, submit_state) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        op: 2,
+        ratings: ratings,
+        text_message: text_message,
+        submit_state: submit_state,
+      },
+      function (response) {
+        if (response) {
+          document.getElementById("response").innerText = "Form Filled";
+        }
+      }
+    );
+  });
+}
 
 // function extract_body(){
 
@@ -206,8 +250,6 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 //   chrome.tabs.sendMessage(tabs[0].id, {extract_body:true}, function(response){
 
 // return response
-     
-  
 
 // });
 
@@ -215,44 +257,31 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
 // }
 
-function fill_forms(ratings,text_message){
+function fill_forms(ratings, text_message, submit_state) {
+  var forms_filled = 0;
 
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; ++i) {
+      chrome.tabs.sendMessage(
+        tabs[i].id,
+        {
+          op: 2,
+          ratings: ratings,
+          text_message: text_message,
+          submit_state: submit_state,
+        },
+        function (response) {
+          console.log(response);
 
-var forms_filled=0
+          if (response) {
+            ++forms_filled;
 
-  chrome.tabs.query({}, function(tabs) {
-
-
-    for (var i=0; i<tabs.length; ++i) {
-     
-
-        chrome.tabs.sendMessage(tabs[i].id, {ratings: ratings,text_message: text_message}, function(response){
-
-          console.log(response)
-          
-          if (response){
-
-
-        
-          ++forms_filled;
-
-          document.getElementById("response").innerText = `${forms_filled} Forms Filled`
-
+            document.getElementById(
+              "response"
+            ).innerText = `${forms_filled} Forms Filled`;
+          }
         }
-
-         
-  
-      });
-
+      );
     }
-
-
-});
-     
-
-    
-
-
-
+  });
 }
-
